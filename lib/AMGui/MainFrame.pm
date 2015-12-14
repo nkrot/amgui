3037ae3623d4f5ef7315022afeae11b84e28fa54
Nikolai Krot <talpus@gmail.com>
2015-12-14 20:58:04 +0100

    class that extends wxGlade generated Base::MainFrame class

--- lib/AMGui/MainFrame.pm
+++ lib/AMGui/MainFrame.pm
@@ -1,24 +1,59 @@
 package AMGui::MainFrame;
 
+#use Wx qw[:allclasses];
+use Wx qw[:everything];
+use Wx::Locale gettext => '_T';
+
 use strict;
 use warnings;
 
-use base 'Wx::Frame';
-
-use Wx;
-use Wx::Event;
-use Wx::XRC;
+use base qw(AMGui::Base::MainFrame);
 
 sub new {
-    my $self = shift->SUPER::new(@_);
+    my( $self, $parent, $id, $title, $pos, $size, $style, $name ) = @_;
+    $self = $self->SUPER::new( $parent, $id, $title, $pos, $size, $style, $name );
 
-    Wx::Event::EVT_BUTTON(
-        $self,
-        Wx::XmlResource::GetXRCID('CloseButton'),
-        sub { $self->Close(1) }
-    );
+    # Have to rebind the handlers here
+    Wx::Event::EVT_MENU($self, wxID_NEW,           \&onFileNew);
+    Wx::Event::EVT_MENU($self, wxID_OPEN,          \&onFileOpen);
+    Wx::Event::EVT_MENU($self, wxID_EXIT,          \&onFileQuit);
+    
+    Wx::Event::EVT_MENU($self, wxID_HELP_CONTENTS, \&onHelpContents);
+    Wx::Event::EVT_MENU($self, wxID_ABOUT,         \&onHelpAbout);
 
     return $self;
 }
+
+sub onFileNew {
+    my ($self, $event) = @_;
+    warn "Event handler (onFileNew) not implemented";
+    $event->Skip;
+}
+
+
+sub onFileOpen {
+    my ($self, $event) = @_;
+    warn "Event handler (onFileOpen) not implemented";
+    $event->Skip;
+}
+
+sub onFileQuit {
+    my ($self, $event) = @_;
+    $self->Close(1);
+}
+
+sub onHelpContents {
+    my ($self, $event) = @_;
+    warn "Event handler (onHelpContents) not implemented";
+    $event->Skip;
+}
+
+
+sub onHelpAbout {
+    my ($self, $event) = @_;
+    warn "Event handler (onHelpAbout) not implemented";
+    $event->Skip;
+}
+
 
 1;
