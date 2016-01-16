4af9414dc6f7c0e398562af01a531f951ceade50
Nikolai Krot <talpus@gmail.com>
2016-01-16 18:52:03 +0100

    a class for constants (wxID_RUN)

---
+++ lib/AMGui/Constants.pm
@@ -0,0 +1,17 @@
+package AMGui::Constants;
+use base qw( Exporter );
+
+#BEGIN { $Exporter::Verbose = 1 }
+
+use strict;
+use warnings;
+
+#our @ISA = qw(Exporter);
+
+our @EXPORT = qw(wxID_RUN SHILO);
+#our @EXPORT_OK = qw( wxID_RUN SHILO );
+
+use constant wxID_RUN => 1000;
+use constant SHILO => 999; # fake
+
+#1;
