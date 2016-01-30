95d948d19fb6d21e9afc4625ced0522b00179135
Nikolai Krot <talpus@gmail.com>
2016-01-30 17:42:02 +0100

    Added NextTag/PreviousTab functionality

--- lib/AMGui/Wx/Notebook.pm
+++ lib/AMGui/Wx/Notebook.pm
@@ -1,5 +1,4 @@
 package AMGui::Wx::Notebook;
-use base qw(Wx::Notebook);
 
 use 5.022000;
 use strict;
@@ -13,37 +12,48 @@ use AMGui::Wx::AuiManager  ();
 
 our $VERSION = '1.00';
 our @ISA     = qw{
-	Wx::AuiNotebook
+    Wx::AuiNotebook
 };
 
+use Class::XSAccessor {
+    getters => {
+        main => 'main'
+    },
+};
+
+#http://docs.wxwidgets.org/trunk/classwx_aui_notebook.html
+
+# TODO
+# 1. after tabs were rearranged by dragging, their retain their initial index.
+#    the effect is that NextTab and PreviousTab are incorrect.
+#    This seems to be a lang-lasting BUG
+# 2. Only a few events are actually dispatched
+
 sub new {
-    my( $class, $parent, $id, $pos, $size, $style, $name ) = @_;
-    $parent = undef              unless defined $parent;
-    $id     = -1                 unless defined $id;
-    $pos    = wxDefaultPosition  unless defined $pos;
-    $size   = wxDefaultSize      unless defined $size;
-    $name   = ""                 unless defined $name;
+    my( $class, $main, $id, $pos, $size, $style, $name ) = @_;
+    $main  = undef              unless defined $main;
+    $id    = -1                 unless defined $id;
+    $pos   = wxDefaultPosition  unless defined $pos;
+    $size  = wxDefaultSize      unless defined $size;
+    $name  = ""                 unless defined $name;
 
-    my $aui = $parent->aui;
+    my $aui = $main->aui;
     
-    #$style = wxNB_TOP unless defined $style;
-
     my @help = (
         "== USAGE ==",
         "Open a file in 'commas' format. File/Open or Ctrl-O",
         "Once it is loaded, double click an item"
     );
 
-    #$self = $self->SUPER::new( $parent, $id, $pos, $size, $style, $name );
-
     my $self = $class->SUPER::new(
-	$parent,
+	$main,
 	-1,
 	Wx::wxDefaultPosition,
 	Wx::wxDefaultSize,
 	Wx::wxAUI_NB_TOP | Wx::wxBORDER_NONE | Wx::wxAUI_NB_SCROLL_BUTTONS | Wx::wxAUI_NB_TAB_MOVE
 		| Wx::wxAUI_NB_CLOSE_ON_ACTIVE_TAB | Wx::wxAUI_NB_WINDOWLIST_BUTTON
     );
+    $self->{main} = $main;
     
     $aui->AddPane(
         $self,
@@ -63,15 +73,33 @@ sub new {
 	
     $aui->caption('notebook' => _T('Hello'), );
 
-#    Wx::Event::EVT_AUINOTEBOOK_PAGE_CHANGED(
-#        $self, $self, 
-#        sub { shift->on_auinotebook_page_changed(@_); }, );
+    Wx::Event::EVT_AUINOTEBOOK_PAGE_CHANGED(
+        $self, $self, 
+        sub { shift->on_auinotebook_page_changed(@_); }, );
 
     #Wx::Event::EVT_AUINOTEBOOK_PAGE_CLOSE(
-#        $parent, $self, 
+#        $main, $self, 
 #        sub { shift->on_close_tab(@_); }, );
 
-   
+    # this event does not happen, thank you, AUINotebook developers
+#    Wx::Event::EVT_AUINOTEBOOK_DRAG_MOTION(
+#        $self, $self, 
+#        sub { shift->on_auinotebook_drag_motion(@_)}, );
+
+    Wx::Event::EVT_AUINOTEBOOK_DRAG_DONE(
+        $self, $self, 
+        sub { shift->on_auinotebook_drag_done(@_)}, );
+
+    # this event does not happen, thank you, AUINotebook developers
+#    Wx::Event::EVT_AUINOTEBOOK_BEGIN_DRAG(
+#        $self, $self, 
+#        sub { shift->on_auinotebook_begin_drag(@_)}, );
+    
+    # this event does not happen, thank you, AUINotebook developers
+#    Wx::Event::EVT_AUINOTEBOOK_END_DRAG(
+#        $self, $self, 
+#        sub { shift->on_auinotebook_end_drag(@_)}, );
+
     $self->{help} = Wx::ListBox->new(
         $self, 
         wxID_ANY, 
@@ -83,7 +111,7 @@ sub new {
 
     $self->create_tab($self->{help}, _T("Usage"));
  
-    $parent->update_aui();
+    $main->update_aui();
 
     return $self;
 }
@@ -97,5 +125,60 @@ sub create_tab {
     return $self->GetSelection;
 }
 
-1;
+sub page_ids {
+    my $self = shift;
+    return ($self->first_page_id..$self->last_page_id);
+}
 
+sub first_page_id {
+    my $self = shift;
+    return 0;
+}
+
+sub last_page_id {
+    my $self = shift;
+    return $self->GetPageCount-1;
+}
+
+sub select_next_tab {
+    my $self = shift;
+    $self->AdvanceSelection();
+    return $self->GetSelection;
+}
+
+sub select_previous_tab {
+    my $self = shift;
+    $self->AdvanceSelection(0);
+    return $self->GetSelection;
+}
+
+######################################################################
+# event handlers
+
+sub on_auinotebook_page_changed {
+    my ($self, $event) = @_;
+    #$self->main->inform("Page Changed");
+    $event->Skip;
+}
+
+#sub on_auinotebook_begin_drag {
+#    my ($self, $event) = @_;
+#    $event->Skip;
+#}
+
+#sub on_auinotebook_end_drag {
+#    my ($self, $event) = @_;
+#    warn @_;
+#    $self->main->inform("End drag");
+#    $event->Skip;
+#}
+
+# happens
+sub on_auinotebook_drag_done {
+    my ($self, $event) = @_;
+#    warn @_;
+#    warn "Current tab id=" . $self->GetSelection;
+    $event->Skip;
+}
+
+1;
