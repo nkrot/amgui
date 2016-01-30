d35b28f7b1b5834f90c5a6bdc8164298b96c6d87
Nikolai Krot <talpus@gmail.com>
2016-01-30 17:46:47 +0100

    added File/Close item

--- lib/AMGui/Wx/Menu/File.pm
+++ lib/AMGui/Wx/Menu/File.pm
@@ -14,8 +14,9 @@ sub new {
     my $self = $class->SUPER::new(@_);
     bless $self, $class;
 
-    $self->Append(Wx::wxID_NEW,  _T("&New\tCtrl+N"), "");
-    $self->Append(Wx::wxID_OPEN, _T("&Open\tCtrl+O"), "");
+    $self->Append(Wx::wxID_NEW,   _T("&New\tCtrl+N"), "");
+    $self->Append(Wx::wxID_OPEN,  _T("&Open\tCtrl+O"), "");
+    $self->Append(Wx::wxID_CLOSE, _T("Close\tCtrl+W"), "");
     $self->AppendSeparator();
     $self->Append(Wx::wxID_EXIT, _T("&Quit\tCtrl+Q"), "");
 
