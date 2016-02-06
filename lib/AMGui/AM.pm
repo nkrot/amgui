c03c06e85d3e8e0601df0ee21f56338b6b7ab353
Nikolai Krot <talpus@gmail.com>
2016-02-06 19:15:08 +0100

    method #classify_all for batch-classifying all items

--- lib/AMGui/AM.pm
+++ lib/AMGui/AM.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Algorithm::AM;
+use Algorithm::AM::Batch;
 use AMGui::Results;
  
 use Class::XSAccessor {
@@ -63,5 +64,57 @@ sub classify {
     $self->{result_viewer}->add( $self->result ); # TODO: do it outside?
     return $self->result;
 }
+
+sub classify_all {
+    my ($self, $testing) = @_;
+    $self->set_testing($testing)  if defined $testing;
+    
+    my $cnt_total = $self->testing->size;
+    my $cnt_correct = 0;
+    
+    $self->{classifier} = Algorithm::AM::Batch->new(
+        training_set  => $self->training->data,
+        end_test_hook => sub {
+            my ($batch, $test_item, $result) = @_;
+            $cnt_correct++  if $result->result eq 'correct';
+
+            my $msg = "Total: " . $cnt_total . "; Correct: " . $cnt_correct;
+            $self->result_viewer->add($result);
+            $self->result_viewer->show_in_statusbar($msg);
+            
+            #print $test_item->comment . ' ' . $result->result . "\n";
+        }
+    );
+
+    my @results = $self->classifier->classify_all( $self->testing->data );
+
+    $self->{result} = 1;
+    
+    return $self->result;
+}
+
+#sub example_from_analogize_pl {
+#    my $batch = Algorithm::AM::Batch->new(
+#        linear => $args{linear},
+#        exclude_given => !$args{include_given},
+#        exclude_nulls => !$args{include_nulls},
+#
+#        training_set => $train,
+#        # print the result of each classification at the time it is provided
+#        end_test_hook => sub {
+#            my ($batch, $test_item, $result) = @_;
+#            ++$count if $result->result eq 'correct';
+#            say $test_item->comment . ":\t" . $result->result . "\n";
+#            for (@print_methods) {
+#                if($_ eq 'gang_detailed'){
+#                    say ${ $result->gang_summary(1) };
+#                }else{
+#                    say ${ $result->$_ };
+#                }
+#            }
+#        }
+#    );
+#    $batch->classify_all($test);
+#}
 
 1;
