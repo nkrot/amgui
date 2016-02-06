b1b31ba6ac6f8eb0ba07f7788adc39c1d6db7cd8
Nikolai Krot <talpus@gmail.com>
2016-02-06 18:59:23 +0100

    replacing #dataset with #training and #testing

--- lib/AMGui/AM.pm
+++ lib/AMGui/AM.pm
@@ -9,26 +9,48 @@ use AMGui::Results;
 use Class::XSAccessor {
     getters => {
         classifier    => 'classifier',
-        dataset       => 'dataset',
+        training      => 'training',      # training dataset
+        testing       => 'testing',       # testing dataset
         result        => 'result',        # last result
         result_viewer => 'result_viewer'  # many results
     },
     setters => {
-        set_dataset       => 'dataset',
         set_result_viewer => 'result_viewer'
     }
 };
 
 sub new {
-    my ($class) = @_;
+    my ($class, $main) = @_;
     my $self = bless {}, $class;
+
+    $self->{main} = $main;
 
     return $self;
 }
 
+sub set_training {
+    my ($self, $dataset) = @_;
+    $self->{training} = $dataset;
+    return $self;
+}
+
+sub set_testing {
+    my ($self, $dataset) = @_;
+    $self->{testing} = $dataset;
+    return $self;
+}
+
+#sub set_datasets {
+#    my ($self, $training, $testing) = @_;
+#    $self->{training} = $training;
+#    $self->{testing}  = $testing;
+#    return $self;
+#}
+
 sub close {
     my $self = shift;
-    $self->{dataset}       = undef;
+    $self->{training}      = undef;
+    $self->{testing}       = undef;
     $self->{result}        = undef;
     $self->{classifier}    = undef;
     $self->{result_viewer} = undef;
@@ -38,9 +60,9 @@ sub close {
 # Classify given test item using preset training set
 sub classify {
     my ($self, $test_item) = @_;
-    $self->{classifier} = Algorithm::AM->new(training_set => $self->{dataset});
-    $self->{result} = $self->classifier->classify( $test_item ); # AM::Result
-    $self->{result_viewer}->add( $self->result );
+    $self->{classifier} = Algorithm::AM->new(training_set => $self->training);
+    $self->{result} = $self->classifier->classify( $test_item ); #=> AM::Result
+    $self->{result_viewer}->add( $self->result ); # TODO: do it outside?
     return $self->result;
 }
 
