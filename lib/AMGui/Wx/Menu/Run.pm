1d11988073dd5188d721b7df2cf03876d69a947d
Nikolai Krot <talpus@gmail.com>
2016-02-25 18:31:14 +0100

    Adding Checkbox <Linear> that acts like a toggle for switching between Linear and Quadratic

--- lib/AMGui/Wx/Menu/Run.pm
+++ lib/AMGui/Wx/Menu/Run.pm
@@ -15,10 +15,17 @@ sub new {
 
     my $self = $class->SUPER::new(@_);
     bless $self, $class;
-
+    
+    # tips are shown in the status bar
+    my %tips = (
+        linear => _T("For computing scores Linear uses occurrences, Quadratic uses pointers."),
+    );
+    
     $self->Append(wxID_RUN_NEXT,   _T("Run AM on the &next item\tCtrl+R"), "");
     $self->Append(wxID_RUN_BATCH,  _T("Run AM on &all items in the current dataset\tCtrl+Shift+R"), "");
-
+    $self->AppendSeparator;
+    $self->AppendCheckItem(wxID_TOGGLE_LINEAR, _T("&Linear"), $tips{linear});
+    
     return $self;
 }
 
