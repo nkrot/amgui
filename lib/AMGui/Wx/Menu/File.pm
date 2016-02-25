d7763f837339b20c073dd5ad48fdee0d0eecce2e
Nikolai Krot <talpus@gmail.com>
2016-02-25 18:30:20 +0100

    nothing

--- lib/AMGui/Wx/Menu/File.pm
+++ lib/AMGui/Wx/Menu/File.pm
@@ -11,7 +11,7 @@ our @ISA = 'Wx::Menu';
 sub new {
     my $class = shift;
     my $main  = shift;
-    
+
     my $self = $class->SUPER::new(@_);
     bless $self, $class;
 
@@ -19,10 +19,10 @@ sub new {
     $self->Append(Wx::wxID_OPEN,      _T("&Open\tCtrl+O"), "");
     $self->Append(wxID_OPEN_PROJECT,  _T("O&pen a Project\tCtrl+Shift+O"), "");
     $self->Append(Wx::wxID_CLOSE,     _T("Close\tCtrl+W"), "");
-    $self->AppendSeparator();
+    $self->AppendSeparator;
     $self->Append(Wx::wxID_SAVE,      _T("&Save\tCtrl+S"), "");
     $self->Append(Wx::wxID_SAVEAS,    _T("Save &As...\tCtrl+Shift+S"), "");
-    $self->AppendSeparator();
+    $self->AppendSeparator;
     $self->Append(Wx::wxID_EXIT,      _T("&Quit\tCtrl+Q"), "");
 
     return $self;
