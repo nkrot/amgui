95e2f173fc5c6f17a0e60e0fac59db48c46f3d31
Nikolai Krot <talpus@gmail.com>
2016-02-25 18:29:48 +0100

    new constant wxID_TOGGLE_LINEAR

--- lib/AMGui/Constant.pm
+++ lib/AMGui/Constant.pm
@@ -1,4 +1,5 @@
 package AMGui::Constant;
+
 use base qw( Exporter );
 
 #BEGIN { $Exporter::Verbose = 1 }
@@ -9,19 +10,22 @@ use warnings;
 #our @ISA = qw(Exporter);
 
 our @EXPORT = qw(
-    wxID_RUN_BATCH wxID_RUN_NEXT
+    wxID_RUN_BATCH wxID_RUN_NEXT wxID_TOGGLE_LINEAR
     wxID_OPEN_PROJECT
     wxID_NEXT_TAB wxID_PREV_TAB
     FALSE TRUE
     MSG_TRAINING_NOT_FOUND
-    );
-#our @EXPORT_OK = qw( wxID_RUN SHILO );
+);
 
-use constant wxID_RUN_BATCH    => 1000;
-use constant wxID_RUN_NEXT     => 1001;
-use constant wxID_OPEN_PROJECT => 1002;
-use constant wxID_NEXT_TAB     => 1003;
-use constant wxID_PREV_TAB     => 1004;
+# in Run menu
+use constant wxID_RUN_BATCH     => 1010;
+use constant wxID_RUN_NEXT      => 1011;
+use constant wxID_TOGGLE_LINEAR => 1012;
+# in File menu
+use constant wxID_OPEN_PROJECT  => 1020;
+# in Window menu
+use constant wxID_NEXT_TAB      => 1030;
+use constant wxID_PREV_TAB      => 1031;
 
 use constant WIN32 => !!( ( $^O eq 'MSWin32' ) or ( $^O eq 'cygwin' ) );
 
