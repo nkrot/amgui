190738a8fe5560dd8ba6fa170529b07866f15150
Nikolai Krot <talpus@gmail.com>
2016-02-26 17:25:56 +0100

    changing the description of [Open a Project]

--- lib/AMGui/Wx/Menu/File.pm
+++ lib/AMGui/Wx/Menu/File.pm
@@ -15,9 +15,15 @@ sub new {
     my $self = $class->SUPER::new(@_);
     bless $self, $class;
 
+    my %tips = (
+        #open_project => _T('Opens a training and a testing datasets at once. Expected file names are "data" and "test"')
+        #open_project => _T('Opens a set of exemplars and a set of test items to be used together in a simulation experiment. Expected file names are "data" and "test"')
+        open_project => _T('Opens a set of exemplars and a set of test items to be used together. Expected file names are "data" and "test"')
+    );
+    
     $self->Append(Wx::wxID_NEW,       _T("&New\tCtrl+N"), "");
     $self->Append(Wx::wxID_OPEN,      _T("&Open\tCtrl+O"), "");
-    $self->Append(wxID_OPEN_PROJECT,  _T("O&pen a Project\tCtrl+Shift+O"), "");
+    $self->Append(wxID_OPEN_PROJECT,  _T("O&pen a Project\tCtrl+Shift+O"), $tips{open_project});
     $self->Append(Wx::wxID_CLOSE,     _T("Close\tCtrl+W"), "");
     $self->AppendSeparator;
     $self->Append(Wx::wxID_SAVE,      _T("&Save\tCtrl+S"), "");
