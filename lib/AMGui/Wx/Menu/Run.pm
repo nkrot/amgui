6c783d828bed04df12879d93a1b0351f7aa5f59b
Nikolai Krot <talpus@gmail.com>
2016-03-01 18:18:00 +0100

    refactoring to use wxID_RUN_* as hash keys

--- lib/AMGui/Wx/Menu/Run.pm
+++ lib/AMGui/Wx/Menu/Run.pm
@@ -18,19 +18,31 @@ sub new {
     
     # tips are shown in the status bar
     my %tips = (
-        linear => _T("For computing scores Linear uses occurrences, Quadratic uses pointers."),
-		include_nulls => _T("If checked, treats null variables in a test item as regular variables."),
-		include_given => _T("If checked, allows a test item to be included in the data set during classification.")
+        wxID_RUN_NEXT             => _T("Classifies the next-after-highlighted item in the testing dataset."),
+        wxID_RUN_BATCH            => _T("Classifies the whole testing dataset"),
+        wxID_TOGGLE_LINEAR        => _T("For computing scores Linear uses occurrences, Quadratic uses pointers."),
+        wxID_TOGGLE_INCLUDE_NULLS => _T("If checked, treats null variables in a test item as regular variables."),
+        wxID_TOGGLE_INCLUDE_GIVEN => _T("If checked, allows a test item to be included in the data set during classification.")
     );
     
-    $self->Append(wxID_RUN_NEXT,   _T("Run AM on the &next item\tCtrl+R"), "");
-    $self->Append(wxID_RUN_BATCH,  _T("Run AM on &all items in the current dataset\tCtrl+Shift+R"), "");
+    $self->Append(wxID_RUN_NEXT,
+                 _T("Run AM on the &next item\tCtrl+R"),
+                 $tips{wxID_RUN_NEXT});
+    $self->Append(wxID_RUN_BATCH,
+                 _T("Run AM on &all items in the current dataset\tCtrl+Shift+R"),
+                 $tips{wxID_RUN_BATCH});
+
     $self->AppendSeparator;
-    $self->AppendCheckItem(wxID_TOGGLE_LINEAR, _T("&Linear"), $tips{linear});
-    $self->AppendCheckItem(wxID_TOGGLE_INCLUDE_NULLS, _T("Include &nulls"), 
-						   $tips{include_nulls});
-    $self->AppendCheckItem(wxID_TOGGLE_INCLUDE_GIVEN, _T("Include &given"), 
-						   $tips{include_given});
+
+    $self->AppendCheckItem(wxID_TOGGLE_LINEAR,
+                           _T("&Linear"),
+                           $tips{wxID_TOGGLE_LINEAR});
+    $self->AppendCheckItem(wxID_TOGGLE_INCLUDE_NULLS,
+                           _T("Include &nulls"), 
+                           $tips{wxID_TOGGLE_INCLUDE_NULLS});
+    $self->AppendCheckItem(wxID_TOGGLE_INCLUDE_GIVEN,
+                           _T("Include &given"), 
+                           $tips{wxID_TOGGLE_INCLUDE_GIVEN});
     
     return $self;
 }
